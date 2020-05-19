c76780c2fb765f9396595c1d3b7d7278adcfaaa0
WutBottle <785012476@qq.com>
2020-05-20 03:02:30 +0800

    许愿树主页

--- src/router/components/Mobile/WishPage/WishCard/WishCard.vue
+++ src/router/components/Mobile/WishPage/WishCard/WishCard.vue
@@ -1,18 +1,31 @@
 <style scoped lang="scss">
   .WishCard {
+    padding: 20px;
+    box-shadow: 0 6px 0 rgb(255, 231, 178), 0 9px 25px rgba(0,0,0,.7);
+    background-color: rgba(255, 255, 255, 0.76);
 
+    border-radius: 12px;
+
+    .text-wrapper {
+      padding-top: 8px;
+      font-size: 16px;
+      color: #3c3c3c;
+      font-weight: bold;
+    }
   }
 </style>
 
 <template>
   <div class="WishCard">
     <van-image
-            width="10rem"
-            height="10rem"
+            width="100%"
+            height="180px"
             fit="contain"
             src="https://img.yzcdn.cn/vant/cat.jpeg"
     />
-    <div></div>
+    <div class="text-wrapper">
+      我的愿望是能有一只喵！！！
+    </div>
   </div>
 </template>
 
--- src/router/components/Mobile/WishPage/WishPage.vue
+++ src/router/components/Mobile/WishPage/WishPage.vue
@@ -1,13 +1,36 @@
 <style scoped lang="scss">
   .WishPage {
+    .main-wrapper {
+      width: 100%;
+      height: calc(100vh - 46px);
+      background-image: url("~@assets/tree.png");
+      background-size: 60%;
+      background-repeat: no-repeat;
+      background-position: center 60%;
+      overflow: auto;
+
+      .card-wrapper {
+        padding: 20px 30px;
+      }
+    }
+
     .plant-tree-wrapper {
       position: fixed;
-      padding: 0 20px 10px 20px;
+      padding: 0 40px 10px 40px;
       width: 100%;
       bottom: 20px;
 
       .van-icon__image {
         display: flex;
+      }
+
+      .func-btn-wrapper {
+        display: flex;
+        justify-content: space-between;
+        padding-bottom: 20px;
+        .btn-style {
+          width: 134px;
+        }
       }
     }
   }
@@ -21,20 +44,34 @@
             left-arrow
             @click-left="onClickLeft"
     />
-    <div class="card-wrapper">
-
+    <div class="main-wrapper">
+      <div class="card-wrapper">
+        <WishCard/>
+      </div>
     </div>
     <div class="plant-tree-wrapper">
+      <div class="func-btn-wrapper">
+        <van-button class="btn-style" type="primary" round size="large"
+                    color="linear-gradient(to right, #ffa298, #ff9cc4)">下一个
+        </van-button>
+        <van-button class="btn-style" type="primary" round size="large"
+                    color="linear-gradient(to right, #ea8bb5, #b9b6ff)">浇水
+        </van-button>
+      </div>
       <van-button style="margin-right: 10px" type="primary" :icon="plant" round size="large"
-                  color="linear-gradient(to right, #ff5559, #ef7f94)">种下种子
+                  color="linear-gradient(to right, #ff5559, #ef7f94)">播下种子
       </van-button>
     </div>
   </div>
 </template>
 
 <script>
+  import WishCard from "./WishCard/WishCard";
   export default {
     name: "WishPage",
+    components: {
+      WishCard,
+    },
     data() {
       return {
         plant: require('@assets/plant.png'),
